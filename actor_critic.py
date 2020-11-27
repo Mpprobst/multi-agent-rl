2fd177a890c7e986b93403e4a8e85a3e7720f5b5
Subash33 <44344741+Subash33@users.noreply.github.com>
2020-11-27 00:40:56 -0500

    actor_critic edited

--- actor_critic.py
+++ actor_critic.py
@@ -1,20 +1,77 @@
 """
 actor_critic.py
-Author:
+Author: Subash Khanal
 Purpose: Implements an actor critic agent
 """
 
-import cnn
+#It is closely based on https://github.com/philtabor/Actor-Critic-Methods-Paper-To-Code
 
-class ACAgent:
-    def __init__(self, env, lr):
+import gym
+import numpy as np
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class AC_NN(nn.Module):
+    def __init__(self, lr, input_size, actions_size, fc1_dims=256, fc2_dims=256):
+        super(AC_NN, self).__init__()
+        self.fc1 = nn.Linear(input_size, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.policy = nn.Linear(fc2_dims, actions_size)
+        self.value = nn.Linear(fc2_dims, 1)
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        policy = self.policy(x)
+        value = self.value(x)
+
+        return (policy, value)
+
+class ACAgent():
+    def __init__(self, lr, input_space, fc1_size, fc2_size, action_space, 
+                 gamma=0.99):
+        
         self.name = "actor_critic"
+        self.lr = lr
+        self.fc1_size = fc1_size
+        self.fc2_size = fc2_size
+        self.actor_critic = AC_NN(lr, input_space, action_space, 
+                                               fc1_size, fc2_size)
+        self.gamma = gamma
+        self.log_prob = None
 
-    def get_action(self, env, state):
-        return 0
+    def action_selection(self, observation):
+        state = T.tensor([observation], dtype=T.float).to(self.actor_critic.device)
+        probabilities, _ = self.actor_critic.forward(state)
+        probabilities = F.softmax(probabilities, dim=1)
+        action_probs = T.distributions.Categorical(probabilities)
+        action = action_probs.sample()
+        log_prob = action_probs.log_prob(action)
+        self.log_prob = log_prob
 
-    def update(self, reward):
-        return 0
+        return action.item()
 
-    def learn(self):
-        return 0
+    def learn(self, state, reward, state_, done):
+        self.actor_critic.optimizer.zero_grad()
+
+        state = T.tensor([state], dtype=T.float).to(self.actor_critic.device)
+        state_ = T.tensor([state_], dtype=T.float).to(self.actor_critic.device)
+        reward = T.tensor(reward, dtype=T.float).to(self.actor_critic.device)
+
+        _, critic_value = self.actor_critic.forward(state)
+        _, critic_value_ = self.actor_critic.forward(state_)
+
+        delta = reward + self.gamma*critic_value_*(1-int(done)) - critic_value
+
+        actor_loss = -self.log_prob*delta
+        critic_loss = delta**2
+
+        (actor_loss + critic_loss).backward()
+        self.actor_critic.optimizer.step()
