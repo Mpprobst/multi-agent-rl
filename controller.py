74f7880e953dabd87fc5ad694bf38d88cce015c3
Michael Probst <mpprobst@gmail.com>
2020-11-26 19:34:43 -0500

    Implement Team Size Argument

--- controller.py
+++ controller.py
@@ -15,15 +15,23 @@ AGENT_MAP = {'reinforce' : reinforce.ReinforceAgent,
              'ac' : actor_critic.ACAgent,
              'random' : random_agent.RandomAgent }
 
+"TODO: add option to include agents on the same team to be different types of agents"
 parser = argparse.ArgumentParser(description='Define the problem to solve.')
 parser.add_argument('--agent1', choices=AGENT_MAP.keys(), default='random', help='Can be ac, reinforce, or random')
 parser.add_argument('--agent2', choices=AGENT_MAP.keys(), default='random', help='Can be ac, reinforce, or random')
+parser.add_argument('--team_size', type=int, default=1, help='The number of agents on each team')
 parser.add_argument('--episodes', type=int, default = 500, help='Number of episodes you want the agent to run.')
 parser.add_argument('--verbose', help='Visualize the environment.', action='store_true')
 args = parser.parse_args()
 
-agent_func1 = AGENT_MAP[args.agent1]
-agent_func2 = AGENT_MAP[args.agent1]
+team1_agents = []
+team2_agents = []
 
-env = soccer_env.SoccerEnv(args.episodes, [agent_func1], [agent_func2], args.verbose)
+for i in range(args.team_size):
+    agent_func1 = AGENT_MAP[args.agent1]
+    agent_func2 = AGENT_MAP[args.agent2]
+    team1_agents.append(agent_func1)
+    team2_agents.append(agent_func2)
+
+env = soccer_env.SoccerEnv(args.episodes, team1_agents, team2_agents, args.verbose)
 #env = double_dunk.DoubleDunk(args.episodes, agent_func, args.verbose)
