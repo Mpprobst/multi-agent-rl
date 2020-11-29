53d8d0dca3ad7e95654f38a5713cb97901cdf145
Subash33 <44344741+Subash33@users.noreply.github.com>
2020-11-29 16:08:25 -0500

    Added sys path to let it run locally

--- controller.py
+++ controller.py
@@ -9,17 +9,19 @@ import double_dunk
 import agents.reinforce as reinforce
 import agents.actor_critic as ac
 import agents.random_agent as random_agent
-import bin.interactive as Interactive
+import sys
+sys.path.append("/Users/subashkhanal/Desktop/Fall_2020/Sequential_Decision_Making/Final_Project/multi-agent-rl/multiagent-particle-envs/bin")
+import interactive as Interactive
 
 AGENT_MAP = {'reinforce' : reinforce.ReinforceAgent,
              'ac' : ac.ACAgent,
              'random' : random_agent.RandomAgent }
 
 parser = argparse.ArgumentParser(description='Define the problem to solve.')
-parser.add_argument('--agent1', choices=AGENT_MAP.keys(), default='random', help='Can be ac, reinforce, or random')
+parser.add_argument('--agent1', choices=AGENT_MAP.keys(), default='ac', help='Can be ac, reinforce, or random')
 parser.add_argument('--agent2', choices=AGENT_MAP.keys(), default='random', help='Can be ac, reinforce, or random')
 parser.add_argument('-s', '--scenario', default='simple.py', help='Path of the scenario Python script.')
-parser.add_argument('--episodes', type=int, default = 500, help='Number of episodes you want the agent to run.')
+parser.add_argument('--episodes', type=int, default = 5, help='Number of episodes you want the agent to run.')
 parser.add_argument('--verbose', help='Visualize the environment.', action='store_true')
 args = parser.parse_args()
 
